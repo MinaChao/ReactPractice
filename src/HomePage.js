2b2deacc8b7d829263e5dabf5df56d42113622af
MinaChao <145426187+MinaChao@users.noreply.github.com>
2024-12-01 18:44:40 +0800

    update

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,8 +8,11 @@ const HomePage = () => {
 				<title>你的網站標題</title>
 				<meta property="og:title" content="你的網站標題" />
 				<meta property="og:description" content="網站的描述文字" />
-				<meta property="og:image" content="https://example.com/your-image.jpg" />
-				<meta property="og:url" content="https://example.com" />
+				<meta
+					property="og:image"
+					content="https://images.plurk.com/7otdT0pziT3uBAe5okMumi.jpg"
+				/>
+				<meta property="og:url" content="https://www.plurk.com/p/pazpim" />
 				<meta property="og:type" content="website" />
 			</Helmet>
 		</div>
